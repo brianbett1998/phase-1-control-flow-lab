6551fc39aa58fda96fc2cf45c328c1d87d2cddc4
BRIAN BETT <+>
2023-03-15 20:34:05 +0300

    Completed

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+  if (someValue <= 400){
+    return "(This is on me!)";
 }
-
+else if (someValue>2000 && someValue<2500)
+{return"I will gladly take your thirty bucks."}
+else if (someValue){
+  return "No can do"
+}
 function ternaryCheckCity(){
   // Write your code here!
+  if (tip==="NYC"){
+    return("No go.")
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip)
   // Write your code here!
-}
+  if (tip==="generous"){
+    return "Thank you so much"
+  }
+  else if (tip==="not as generous"){
+    return "Thank you."}
+    else{
+      return"Bye"
+    }
+  }
